5837f59520fa9f05ee60b91441ae149098849a02
Ricardo Yegros <ricardo_yegros@hotmail.com>
2023-05-13 17:05:27 -0300

    responsive in skills page added

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -3,7 +3,7 @@ import {useDarkModeContext} from "../contexts/darkModeContext";
 export default function Skill({name, x, y}){
     const {darkMode} = useDarkModeContext();
     return(
-        <motion.div className={`flex items-center justify-center rounded-full font-semibold font-montserrat  bg-dark text-light  py-3 px-6 shadow-dark cursor-pointer absolute border-2 boder-solid ${!darkMode ? "border-dark" : "border-light"} `} whileHover={{
+        <motion.div className={`flex items-center justify-center rounded-full font-semibold font-montserrat  bg-dark py-3 px-6 shadow-dark cursor-pointer absolute border-2 boder-solid text-light ${!darkMode ? "border-dark xs:text-dark" : "border-light xs:text-light md:text-light"} lg:py-2 lg:px-4 md:text-sm md:py-1.5 md:px-3 xs:bg-transparent xs:font-bold xs:border-none`} whileHover={{
             backgroundColor:["#121212", "rgba(131,58,180,1)","rgba(253,29,29,1)","rgba(252,176,69,1)","rgba(131,58,180,1)", "#121212"],
             transition:{duration:1, repeat: Infinity},scale:1.05
            }} initial={{x:0,y:0}} whileInView={{x:x, y:y}} transition={{duration:1.5}} viewport={{once:true}}>
--- src/pages/SkillsPage/SkillsPage.jsx
+++ src/pages/SkillsPage/SkillsPage.jsx
@@ -7,10 +7,10 @@ export default function SkillsPage() {
   const {darkMode} = useDarkModeContext();
   return (
     <Layout className="pt-12">
-      <AnimatedText text="Mis Habilidades" className="!text-7xl" />
-      <div className={`w-full h-screen relative flex items-center justify-center rounded-full ${!darkMode ? "bg-circularLight" : "bg-circularDark"}`}>
+      <AnimatedText text="Mis Habilidades" className="!text-7xl md:!text-6xl sm:!text-4xl" />
+      <div className={`w-full h-screen xs:max-h-96 relative flex items-center justify-center rounded-full ${!darkMode ? "bg-circularLight lg:bg-circularLightLg md:bg-circularLightMd sm:bg-circularLightSm" : "bg-circularDark lg:bg-circularDarkLg md:bg-circularDarkMd sm:bg-circularDarkSm"}`}>
         <motion.div
-          className={`flex items-center justify-center rounded-full font-semibold font-montserrat bg-dark text-light ${!darkMode ? "border-dark" : "border-light"} border-2 border-solid shadow-dark p-8 cursor-pointer`}
+          className={`flex items-center justify-center rounded-full font-semibold font-montserrat bg-dark text-light ${!darkMode ? "border-dark xs:text-dark" : "border-light "} border-2 border-solid shadow-dark p-8 cursor-pointer xs:border-none lg:p-6 md:p-4 xs:text-xs xs:p-2 xs:bg-transparent`}
           whileHover={{
             backgroundColor: [
               "#121212",
@@ -26,15 +26,15 @@ export default function SkillsPage() {
         >
           Web
         </motion.div>
-        <Skill name="Next.js" x="-19vw" y="2vw" />
-        <Skill name="Express" x="-5vw" y="-11vw" />
-        <Skill name="JavaScript" x="23vw" y="6vw" />
+        <Skill name="Next.js" x="-22vw" y="0vw" />
+        <Skill name="Express" x="0vw" y="-11vw" />
+        <Skill name="JavaScript" x="21vw" y="12vw" />
         <Skill name="ReactJS" x="0vw" y="11vw" />
-        <Skill name="Redux" x="-20vw" y="-15vw" />
-        <Skill name="NodeJS" x="15vw" y="-12vw" />
-        <Skill name="PostgreSQL" x="32vw" y="-5vw" />
-        <Skill name="Vue.js" x="0vw" y="-19vw" />
-        <Skill name="TailwindCSS" x="-32vw" y="3vw" />
+        <Skill name="Redux" x="-18vw" y="-11vw" />
+        <Skill name="NodeJS" x="23vw" y="-15vw" />
+        <Skill name="PostgreSQL" x="22vw" y="0vw" />
+        <Skill name="Vue.js" x="0vw" y="-23vw" />
+        <Skill name="TailwindCSS" x="-23vw" y="15vw" />
       </div>
     </Layout>
   );
